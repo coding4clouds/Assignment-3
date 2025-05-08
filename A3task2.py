fce31ef5e14d68d75a56ef0352fc257d08f663fa
coding4clouds <kumaririddhi4231@gmail.com>
2025-05-08 21:19:42 +0530

    Add files via upload

---
+++ A3task2.py
@@ -0,0 +1,12 @@
+#task2 using the math module for calculations
+
+import math as m
+num=float(input('Enter a number: '))
+square_root=m.sqrt(num)
+log=m.log(num,m.e)
+sine=m.sin(m.radians(num))
+print('Square root: ',square_root)
+print('Logarithm: ',log)
+print('Sine: ',sine)
+
+
